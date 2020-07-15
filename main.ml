5890261754d44a786193003bf567cfbc441ed206
Jake Nieto <jnieto@Jakes-MBP.fios-router.home>
2020-07-15 18:23:38 -0400

    added divide and undef type

--- main.ml
+++ main.ml
@@ -1,47 +1,62 @@
-type nat = Zero | Succ of nat
+
+type nat = Zero | Succ of nat | Undef
 
 let rec int_of_nat nat = match nat with 
+  | Undef -> raise (Failure("undefined"))
   | Zero -> 0
   | Succ(child) -> int_of_nat child + 1 
 
-let my_nat = Succ(Succ(Zero))
-
 let rec nat_of_int n = match n with
   | 0 -> Zero
-  | n -> if n > 0 
-         then Succ(nat_of_int(n-1))
-         else raise (Failure("invalid input: negative numbers are invalid"))
-
+  | n when n > 0 -> Succ(nat_of_int(n-1))
+  | n -> Undef
+        
 (* type ('a,'b) result = Error of 'a | Ok of 'b  *)
 
-(* 
-let rec nat_of_int_result n = match n with
-  | 0 -> Ok(Zero)
-  | n -> if n > 0 
-         then Ok(Succ(nat_of_int_result(n-1)))
-         else Error(Failure("invalid input: negative numbers are invalid")) *)
+let rec nat_of_int_result n = 
+  if n < 0 
+    then Ok(nat_of_int n)
+    else Error(Failure("invalid input: negative numbers are invalid")) 
 
 let rec nat_add a b = match (a,b) with 
+  | (Undef, _)
+  | (_,Undef) ->  Undef
   | (a, Zero)
   | (Zero, a) -> a
   | (Succ(c), Succ(d)) ->  Succ(Succ(nat_add c d))
 
 let rec nat_mult a b = match (a,b) with 
-  | (a, Zero)
-  | (Zero, a) -> Zero
+  | (Undef, _)
+  | (_,Undef) ->  Undef
+  | (_, Zero)
+  | (Zero, _) -> Zero
   | (a, Succ(c)) -> nat_add a (nat_mult a c )
-(* 
-let rec nat_sub a b = match (a,b) with 
-  | (a, Zero) -> Ok(a)
-  | (Succ(c), Succ(d)) -> Ok(nat_sub c d)
-  | (Zero, a) -> Error(Failure("invalid input: negative result")) *)
 
+let rec nat_sub a b = match (a,b) with
+  | (Undef, _)
+  | (_, Undef) ->  Undef
+  | (a, Zero) -> a
+  | (Succ(c), Succ(d)) -> nat_sub c d
+  | (Zero, _) -> Undef
+  
 let rec nat_to_string n = match n with 
+  | Undef -> "Undef"
   | Zero -> "Zero"
   | Succ(a) -> "Succ(" ^ nat_to_string(a) ^ ")"
 
+let rec nat_divide a b = match (a, b) with
+| (Undef, _)
+| (_, Undef) ->  Undef
+| (_, Zero) -> Undef
+| (Zero, _) -> Zero
+| (c, d) -> 
+    let diff = nat_sub c d  in 
+      match diff with 
+        | Undef -> Zero
+        | nat -> Succ(nat_divide (nat_sub c d) d)
 
-let () = Printf.printf "Answer: %d\n" (int_of_nat my_nat)
+(* let () = Printf.printf "Answer: %d\n" (int_of_nat my_nat) *)
 let () = Printf.printf "Answer: %s\n" (nat_to_string (nat_add (nat_of_int 2) (nat_of_int 3)))
+
 
 
--- test.ml
+++ test.ml
@@ -1,5 +1,6 @@
 open OUnit2
 open ExtLib
+open Main
 
 let t_any ?cmp:(cmp=(=)) ?printer:(printer=dump) (name : string) expected value = name>::
   (fun _ -> assert_equal expected value ~printer:printer ~cmp:cmp);;
@@ -7,12 +8,20 @@ let t_any ?cmp:(cmp=(=)) ?printer:(printer=dump) (name : string) expected value 
 let t_any_err (name : string) (runnable : unit -> 'a) (expected_err : exn) = name>::
   (fun _ -> assert_raises expected_err runnable)
 
-let t_int_of_nat    
+let t_int_of_nat (name : string) (n: nat) (expected: int) = t_any name expected (int_of_nat n)
+  
 let suite = "suite">:::[
     t_any "test" 2 (1+1);
     t_any_err "test-error" (fun () -> failwith "rip") (Failure("rip"));
 ]
 
+let int_of_nat_tests = "int_of_nat_tests">:::[
+    t_int_of_nat "zero" Zero 0;
+    t_int_of_nat "one" (Succ(Zero)) 0;
+]
+
 
 let () = 
-    run_test_tt_main suite;
+    run_test_tt_main suite; 
+    run_test_tt_main int_of_nat_tests
+    
